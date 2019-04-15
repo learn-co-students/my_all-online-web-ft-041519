46e692a8f48ea7e1f422da446cb864d7f621ad49
James Pier <jpier2012@gmail.com>
2019-04-15 13:20:44 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  new_array = []
+  collection.each {|element|
+    if yield(element)
+      new_array << element
+    end
+  }
+  new_array == collection ? true : false
 end
